48e0636eced632dd46e34729baed2647b8156878
nanyssalazar <andrea.sosa01@iest.edu.mx>
2020-12-21 15:33:49 -0600

    SFO-65: Nav Bar

---
+++ andrea/js/app.js
@@ -0,0 +1,10 @@
+var signUp = document.querySelector("#sign-btn");
+var logIn = document.querySelector("#login-btn");
+
+signUp.addEventListener("click", () => {
+    console.log("Sign Up clicked");
+})
+
+logIn.addEventListener("click", () => {
+    console.log("Log In clicked")
+})
