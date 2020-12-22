20fa2b025236b8181094e4d0b738b7780bcb87e3
Josué Arreola <josuearreola@pop-os.localdomain>
2020-12-21 18:58:12 -0600

    SFO-67: Create navbar

---
+++ josue/js/app.js
@@ -0,0 +1,11 @@
+const signInBtn = document.querySelector("#sign");
+const register = document.querySelector("#register");
+
+signInBtn.addEventListener("click",e=>{
+    console.log(e)
+    console.log("You clicked sign in !!!!!")
+})
+register.addEventListener("click",e=>{
+    console.log(e);
+    console.log("YOu clicked register button!!!!");
+})
