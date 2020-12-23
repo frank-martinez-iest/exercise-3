3b6c78e61d2258dfc32c542626bad466bbf15f5d
Josue Arreola <wiijosue333@gmail.com>
2020-12-22 18:40:15 -0600

    Remove event log

--- josue/js/app.js
+++ josue/js/app.js
@@ -1,11 +1,9 @@
 const signInBtn = document.querySelector("#sign");
 const register = document.querySelector("#register");
 
-signInBtn.addEventListener("click",e=>{
-    console.log(e)
+signInBtn.addEventListener("click",()=>{
     console.log("You clicked sign in !!!!!")
 })
-register.addEventListener("click",e=>{
-    console.log(e);
+register.addEventListener("click",()=>{
     console.log("YOu clicked register button!!!!");
 })
