797fe0d6910d3d7f0afb448904ce9eefc280dc60
Josue Arreola <wiijosue333@gmail.com>
2020-12-26 14:33:59 -0600

    SFO-81: Create register form

--- josue/js/app.js
+++ josue/js/app.js
@@ -1,6 +1,17 @@
 const signInBtn = document.querySelector("#sign");
 const register = document.querySelector("#register");
+const registerForm = {} ;
 
+const registerUser = () =>{
+    event.preventDefault();
+    console.log("User is now registered");
+}
+const handleChange = (e)=>{
+    const inputField = e.target.id;
+    const inputValue = e.target.value;
+    registerForm[inputField] = inputValue;
+    console.table(registerForm)
+}
 signInBtn.addEventListener("click",()=>{
     console.log("You clicked sign in !!!!!")
 })
