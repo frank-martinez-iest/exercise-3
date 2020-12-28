95781b0cfd9c313d41eff4e4694e0e6e369bfa58
Josue Arreola <wiijosue333@gmail.com>
2020-12-28 02:33:10 -0600

    SFO-81: Create a form validator, change dynamically sign and register form

--- josue/js/app.js
+++ josue/js/app.js
@@ -1,20 +1,29 @@
-const signInBtn = document.querySelector("#sign");
-const register = document.querySelector("#register");
-const registerForm = {} ;
+import FormValidator from './Formvalidator.js';
 
-const registerUser = () =>{
-    event.preventDefault();
-    console.log("User is now registered");
+const signInBtn = document.querySelector("#signBtn");
+const registerBtn = document.querySelector("#registerBtn");
+const submitBtn = document.querySelector("#submit");
+const registerFields = document.querySelector("#register");
+let fields = ['name','phone']
+
+const createValidator = (fields) =>{
+    const form = document.querySelector('.form');
+    const validator = new FormValidator(form,fields);
+    validator.initialize();
 }
-const handleChange = (e)=>{
-    const inputField = e.target.id;
-    const inputValue = e.target.value;
-    registerForm[inputField] = inputValue;
-    console.table(registerForm)
-}
-signInBtn.addEventListener("click",()=>{
-    console.log("You clicked sign in !!!!!")
+signInBtn.addEventListener("click",e=>{
+    registerFields.classList.add("register--hidden");
+    submitBtn.innerText = "Sign In"
+    fields = ['name','phone']
+    createValidator(fields)
+
 })
-register.addEventListener("click",()=>{
-    console.log("YOu clicked register button!!!!");
+registerBtn.addEventListener("click",e=>{
+    registerFields.classList.remove("register--hidden");
+    submitBtn.innerText = "Register"
+    fields = ['name','phone','email','password','password_confirmation']
+    createValidator(fields)
+
 })
+
+createValidator(fields)
