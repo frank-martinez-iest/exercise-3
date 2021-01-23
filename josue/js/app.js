593c82541fd1cc6c9719d03b4b7c028c3b5ebdaa
Josue Arreola <wiijosue333@gmail.com>
2021-01-23 12:50:32 -0600

    Set initial balance to already registered users

--- josue/js/app.js
+++ josue/js/app.js
@@ -8,28 +8,28 @@ const registerFieldsWrapper = document.querySelector("#register-fields");
 const boardWrapper = document.querySelector(".account__board-wrapper");
 const userDashboard = document.querySelector(".account__board-user");
 const welcomeSplash = document.querySelector('.account__splash-img');
-const accountSection =document.querySelector(".account");
-const fields = ['name','phone'];
-const registerFieldsIds = ['email','password','password_confirmation'];
+const accountSection = document.querySelector(".account");
+const fields = ['name', 'phone'];
+const registerFieldsIds = ['email', 'password', 'password_confirmation'];
 const registrationFormWrapper = document.querySelector('.account__left-wrapper');
 /*STRINGS*/
 const SIGN_IN = 'Sign In';
 const REGISTER = 'Register';
 
-const addRegisterFieldsToValidator = () =>{
+const addRegisterFieldsToValidator = () => {
     // First step, combine the actual validator fields with the registerFields
-    const fieldsCombined = [...validator.fields,...registerFieldsIds];
+    const fieldsCombined = [...validator.fields, ...registerFieldsIds];
     // We have to filter the array to delete the repeated elements 
     const newFilteredFormArray = [...new Set(fieldsCombined)]
     validator.fields = newFilteredFormArray;
-    registerFieldsIds.forEach(field=>{
+    registerFieldsIds.forEach(field => {
         const input = document.querySelector(`#${field}`);
-        input.addEventListener('input',()=>{
+        input.addEventListener('input', () => {
             validator.validateFields(input);
         });
     });
 }
-const parseFormData= ()=>{
+const parseFormData = () => {
     const myObjectFromForm = {}
     validator.fields.forEach(field => {
         const input = document.querySelector(`#${field}`)
@@ -39,57 +39,52 @@ const parseFormData= ()=>{
     return myObjectFromForm
 }
 
-signInBtn.addEventListener("click",()=>{
+signInBtn.addEventListener("click", () => {
     registerFieldsWrapper.remove();
     submitBtn.innerText = SIGN_IN;
-    validator.fields = validator.fields.slice(0,2);
+    validator.fields = validator.fields.slice(0, 2);
 });
 
-registerBtn.addEventListener("click",()=>{
-    form.insertBefore(registerFieldsWrapper,submitBtn);
+registerBtn.addEventListener("click", () => {
+    form.insertBefore(registerFieldsWrapper, submitBtn);
     submitBtn.innerText = REGISTER;
     addRegisterFieldsToValidator();
 });
 
-submitBtn.addEventListener('click',e=>{
+submitBtn.addEventListener('click', e => {
     e.preventDefault();
     const isFormValid = validator.validateOnSubmit();
-    if(isFormValid){
+    if (isFormValid) {
         // Fields are filled correctly now we check if user wants to register or login
-        if(submitBtn.innerText == SIGN_IN){
+        if (submitBtn.innerText == SIGN_IN) {
             const userData = parseFormData();
             const user = JSON.parse(window.localStorage.getItem(`${userData.phone}`));
-            console.log(user);
-            if(user){
-                welcomeSplash.remove();
-                registrationFormWrapper.remove();
-                accountSection.classList.remove('account');                
-                accountSection.classList.add('dashboard');                
-                accountSection.appendChild(boardWrapper);
-                loadUserDashboard(user);
-            }else{
-                alert("You are not part of BankApp, want to register instead?");
-            }
+            user ? loadUserDashboard(user) : alert("You are not an user, you have to register first!");
         }
-        else{
+        else {
             // User wants to register.. save it to localstorage
-            const  userData = parseFormData();
-            window.localStorage.setItem(`${userData.phone}`,JSON.stringify(userData));
+            const userData = parseFormData();
+            window.localStorage.setItem(`${userData.phone}`, JSON.stringify(userData));
             confirm("You are now part of BankApp go to Sign In!");
         }
     }
 });
 
-const loadUserDashboard = (user)=>{
+const loadUserDashboard = (user) => {
+    welcomeSplash.remove();
+    registrationFormWrapper.remove();
+    accountSection.classList.remove('account');
+    accountSection.classList.add('dashboard');
+    accountSection.appendChild(boardWrapper);
+    user.balance ?? setInitialUserBalance(user);
     userDashboard.firstElementChild.innerHTML = `Welcome ${user.email}`;
-    console.log(userDashboard.querySelector('#current-balance').innerText);
-    userDashboard.querySelector('#current-balance').innerText = `Your current balance is ${parseNumberToLocateString(user.balance)}`
+    userDashboard.querySelector('#current-balance').innerText = `Your current balance is ${parseNumberToLocaleString(user.balance)}`
 }
+const setInitialUserBalance = (user) => window.localStorage.setItem(`${user.phone}`, JSON.stringify({ ...user, 'balance': 0 }));
+const parseNumberToLocaleString = (number) => number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
-const parseNumberToLocateString = (number)=> number.toLocaleString('en-US',{style : 'currency',currency:'USD'}
 
-)
 registerFieldsWrapper.remove();
 boardWrapper.remove();
-const validator = new FormValidator(form,fields);
+const validator = new FormValidator(form, fields);
 validator.initialize()
